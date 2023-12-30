2f6d0e86df20ab8a37f66a1310e06cc3f4c41422
Muhammad Fahid <ranafahadirshad@gmail.com>
2023-12-31 01:42:02 +0500

    modules

---
+++ 2-FileModule.js
@@ -0,0 +1,15 @@
+// File system Module allows to interact with files on system
+
+
+// // Asynchronous Method
+// const fs = require('fs')
+
+// const path = './Content/sample.txt'
+
+// fs.readFile(path,'utf8',(err,data)=>{
+//     if(err){
+//         console.log("Error : ",err)
+//         return;
+//     }
+//     console.log("FileContent:" , data)
+// })
--- 2-osModule.js
+++ 2-osModule.js
@@ -1,6 +1,20 @@
 //we can access os through modules
 const os = require('os')
 
+// This will give you info about system
 const user = os.userInfo()
-
 console.log(user)
+
+
+// To know abut the system uptime
+console.log(`systm uptime is ${os.uptime()} seconds`)
+
+//to know more about system information
+
+const metaData= {
+    name :os.type(),
+    release:os.release(),
+    totalMem:os.totalmem,
+}
+
+console.log(`Name : ${metaData.name} , ReleaseDate: ${metaData.release} ,TotalMemory : ${metaData.totalMem} `)
