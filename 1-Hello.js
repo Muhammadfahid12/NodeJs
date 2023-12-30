d65df6f5a7a2d0fb8208418c17a8c83bf334f21c
Muhammad Fahid <ranafahadirshad@gmail.com>
2023-12-30 21:35:01 +0500

    Intro to Modules

---
+++ 1-Hello.js
@@ -0,0 +1,8 @@
+// console.log("Hello World From NodeJS")
+
+let h = () => {
+    console.log("faisalabad is city of pakistan")
+}
+
+
+h()
---
+++ 2-osModule.js
@@ -0,0 +1,6 @@
+//we can access os through modules
+const os = require('os')
+
+const user = os.userInfo()
+
+console.log(user)
